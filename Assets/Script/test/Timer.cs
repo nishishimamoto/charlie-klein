85695fb5351e6508318ac5c2ddcb80e80a321074
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-03-24 22:56:39 +0900

    タイマーの処理追加

--- Assets/Script/test/Timer.cs
+++ Assets/Script/test/Timer.cs
@@ -5,29 +5,53 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    static float maxTime = 30.0f;
+
     Text timerText;
-    public float timeCount = 30.0f;            //制限時間
+    public float timeCount;            //制限時間
     public bool timeOut;
     public bool countStart;
+
+    [SerializeField] Image timerSlider;
+    [SerializeField] Text bigTimerText;
 
     // Start is called before the first frame update
     void Start()
     {
         timerText = GetComponent<Text>();
-        timerText.text = timeCount.ToString("f1");  //時間の表示
+        timeCount = maxTime;
+        timerText.text = Mathf.Ceil(timeCount).ToString("f0");  //時間の表示
+        bigTimerText.enabled = false;   //5秒前から表示
     }
 
     public void TimerCount()
     {
-        timerText.text = timeCount.ToString("f1");  //時間の表示
+        timerText.text = Mathf.Ceil(timeCount).ToString("f0");  //時間の表示
+        timerSlider.fillAmount = timeCount / maxTime;    //円画像の表示
+        ChangeSliderColor();    //15,5秒で円画像の色変更
+
         if (timeCount > 0 && countStart)
         {
             timeCount -= Time.deltaTime;    //制限時間のカウントダウン
-
-        }else if (timeCount <= 0)
+            if(timeCount <= 5)
+            {
+                bigTimerText.enabled = true;   //5秒前から表示
+                bigTimerText.text = Mathf.Ceil(timeCount).ToString("f0");  //時間の表示
+            }
+        }
+        else if (timeCount <= 0)
         {
             timeOut = true;
             countStart = false;
+            bigTimerText.enabled = false;
+            timeCount = 0;
         }
+    }
+
+    void ChangeSliderColor()
+    {
+            if (timeCount <= 5 && timerSlider.color == Color.yellow)timerSlider.color = Color.red;
+            else if (timeCount <= 15 && timerSlider.color == Color.white) timerSlider.color = Color.yellow;
+            else if (timeCount >= 15 && timerSlider.color == Color.red) timerSlider.color = Color.white;
     }
 }

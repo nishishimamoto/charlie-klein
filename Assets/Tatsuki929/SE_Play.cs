0943f03dc254f1f370ecb8d6cdfdc1f029a60986
tatsuki Isagawa <11807113@stu.kbc.ac.jp>
2021-03-25 14:52:45 +0900

    ・SEを追加 ・Audiomixerでの音量調整 ・SE / BGMをプレハブ化

---
+++ Assets/Tatsuki929/SE_Play.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SE_Play : MonoBehaviour
+{
+    public AudioSource SE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SE = this.GetComponent<AudioSource>();
+
+        SE.volume = 1;
+
+        SE.clip = Resources.Load<AudioClip>("Earth_Tremor");
+
+       // SE.outputAudioMixerGroup = Resources.Load<AudioMixerGroup>("T_Audiomixer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("LB")|| Input.GetButtonDown("RB"))
+        {
+            SE.PlayOneShot(SE.clip);
+        }
+    }
+}

de20a7de8fda99c83b82c86ca77c7b9fcf785a2b
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-04-13 23:44:34 +0900

    別ルール(演出途中)

---
+++ Assets/nishi/teststages/TurnOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOver : MonoBehaviour
+{
+    public int lifeSpan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeSpan = 3;
+        GetComponent<TextMesh>().text = "" + lifeSpan;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonDown("Y")) LifeCountDown();
+        //if (Input.GetButtonDown("X")) LifeCountReSet();
+    }
+
+    public void LifeCountDown()
+    {
+        lifeSpan -= 1;
+
+        if(lifeSpan == 2) GetComponent<TextMesh>().color = Color.yellow;//黄
+        else if (lifeSpan == 1) GetComponent<TextMesh>().color = Color.red;//赤
+
+        GetComponent<TextMesh>().text = "" + lifeSpan;
+    }
+
+    public void LifeCountReSet()
+    {
+        lifeSpan = 3;
+        GetComponent<TextMesh>().color = Color.white;//白
+        GetComponent<TextMesh>().text = "" + lifeSpan;
+    }
+}

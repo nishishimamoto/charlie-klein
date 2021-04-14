1c24319e5e56020ecea48237faddda6a81d48263
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-04-14 15:02:38 +0900

    フォルダの移動など

---
+++ Assets/nishi/test3/test3score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test3score : MonoBehaviour
+{
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "" + test3.score;
+    }
+}

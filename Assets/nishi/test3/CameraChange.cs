d5df83e082f00f36149239557d43737990b6ab74
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-05-06 10:37:52 +0900

    ゲームオーバーのテキストとターンのテキストの裏に薄い黒パネルを置いて文字を読みやすくした

--- Assets/nishi/test3/CameraChange.cs
+++ Assets/nishi/test3/CameraChange.cs
@@ -4,35 +4,25 @@ using UnityEngine;
 
 public class CameraChange : MonoBehaviour
 {
-    private GameObject mainCamera;      //メインカメラ格納用
-    private GameObject subCamera;       //サブカメラ格納用 
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject subCamera;
     public bool isParfectCamera;
 
-    //呼び出し時に実行される関数
+
     void Start()
     {
-        //メインカメラとサブカメラをそれぞれ取得
-        mainCamera = GameObject.Find("Main Camera");
-        subCamera = GameObject.Find("SubCamera");
 
-        //サブカメラを非アクティブにする
-        subCamera.SetActive(false);
     }
 
-
-    //単位時間ごとに実行される関数
     void Update()
     {
-        //スペースキーが押されている間、サブカメラをアクティブにする
         if (isParfectCamera)
         {
-            //サブカメラをアクティブに設定
             mainCamera.SetActive(false);
             subCamera.SetActive(true);
         }
-        else
+        else if (!isParfectCamera)
         {
-            //メインカメラをアクティブに設定
             subCamera.SetActive(false);
             mainCamera.SetActive(true);
         }

edcf0b7c4c4546055cc8ce469d1ff6f55c1dc5d9
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-05-18 14:15:11 +0900

    タイトルアニメーション

---
+++ Assets/nishi/test3/Script/TitleLogo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleLogo : MonoBehaviour
+{
+    [SerializeField] GameObject titleLogo;
+    [SerializeField] GameObject buttons;
+    int logoSize;
+    float animTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleLogo.transform.localScale = new Vector3(394, 0, 1);
+        buttons.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animTime += Time.deltaTime;
+        if (logoSize <= 218 && animTime >= 0.5f)
+        {
+            logoSize++;
+            titleLogo.transform.localScale = new Vector3(394, logoSize, 1);
+        }
+        else if(animTime >= 1.8f)
+        {
+            buttons.SetActive(true);
+        }
+    }
+}

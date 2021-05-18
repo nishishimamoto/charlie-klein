6991df91a8052fd3220c4adaa44dafc7735a9845
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-05-18 17:08:52 +0900

    タイトルロゴの速度修正

--- Assets/nishi/test3/Script/TitleLogo.cs
+++ Assets/nishi/test3/Script/TitleLogo.cs
@@ -6,7 +6,7 @@ public class TitleLogo : MonoBehaviour
 {
     [SerializeField] GameObject titleLogo;
     [SerializeField] GameObject buttons;
-    int logoSize;
+    float logoSize;
     float animTime = 0;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,8 @@ public class TitleLogo : MonoBehaviour
         animTime += Time.deltaTime;
         if (logoSize < 218 && animTime >= 0.5f)
         {
-            logoSize += 4;
+            logoSize = animTime * 130;
+            if (logoSize > 218) logoSize = 218;
             titleLogo.transform.localScale = new Vector3(394, logoSize, 1);
         }
         else if(animTime >= 1.8f)

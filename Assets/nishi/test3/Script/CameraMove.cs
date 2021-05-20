385f5a99c07c56be8e45e50a0131756099219eb1
nishishimamoto <11807020@stu.kbc.ac.jp>
2021-05-20 18:33:58 +0900

    ビルドタイムスタンプ消去、サイクルサテライト全消し時の演出追加、デバッグモード消去

--- Assets/nishi/test3/Script/CameraMove.cs
+++ Assets/nishi/test3/Script/CameraMove.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // 中心点
-    [SerializeField] private Vector3 _center = Vector3.zero;
+    Camera camera;
+    Vector3 center = Vector3.zero;
+    Vector3 axis = new Vector3(0,0,-1);
+    float speed = 7;
 
-    // 回転軸
-    [SerializeField] private Vector3 _axis = Vector3.left;
+    float cameraTime = 0;
+    Vector3 pos;
 
-    // 円運動周期
-    [SerializeField] private float _period = 2;
-
-    private void Update()
+    void Start()
     {
-        // 中心点centerの周りを、軸axisで、period周期で円運動
-        transform.RotateAround(
-            _center,
-            _axis,
-            360 / _period * Time.deltaTime
-        );
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (camera.enabled)
+        {
+            transform.RotateAround(center, axis, (360 / speed) * Time.deltaTime);
+
+            cameraTime += Time.deltaTime;
+            pos = transform.position;
+            if (cameraTime >= 2 && pos.z >= -12) transform.Translate(0, 0, -0.15f);
+            else if (cameraTime >= 1.65f && pos.z <= -4) transform.Translate(0, 0, 0.25f);
+        }
+        if(!camera.enabled && cameraTime > 0)
+        {
+            cameraTime = 0;
+            pos.z = -12;
+        }
     }
 }

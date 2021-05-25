78c5c9dc245b9edd0404cc657b625848fed5f836
tatsuki Isagawa <11807113@stu.kbc.ac.jp>
2021-05-25 14:16:39 +0900

    音量をセーブしなかった際、ファイルから音量を呼び出して変更前のサイズに戻します

--- Assets/Tatsuki929/Option.cs
+++ Assets/Tatsuki929/Option.cs
@@ -58,18 +58,6 @@ public class Option : MonoBehaviour
     void Update()
     {
 
-        if (vol_BGM <= -30)
-        {
-            
-            source_BGM = true;
-
-        }
-        if (vol_SE <= -30)
-        {
-            
-            source_SE = true;
-
-        }
         if (!isBack && !isGraph && !isSound)
         {
             obj_Sound.SetActive(true);
@@ -270,6 +258,15 @@ public class Option : MonoBehaviour
             if (cursol == 0&&isSound)
             {
                cursol2 = 0;
+
+                using (StreamReader sr = new StreamReader(Application.dataPath + "/Resources/Volume"))
+                {
+                    vol_BGM = float.Parse(sr.ReadLine());
+                    vol_SE = float.Parse(sr.ReadLine());
+                }
+                mixer.SetFloat("SE", vol_SE);
+                mixer.SetFloat("BGM", vol_BGM);
+
                 isSound = false;
                 gameSECS.audioSource.PlayOneShot(gameSECS.crickSE);
             }

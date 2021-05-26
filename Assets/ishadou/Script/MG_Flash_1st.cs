7db5b578610583cc79e1f9fd356f62ac2436b88e
CG16-09-PC\GP4_11807128 <11807128@stu.kbc.ac.jp>
2021-05-26 14:06:05 +0900

    CS

---
+++ Assets/ishadou/Script/MG_Flash_1st.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MG_Flash_1st : MonoBehaviour
+{
+    [SerializeField] Isha_Singlshot ishaCS;
+    [SerializeField] GameSE gameSECS;
+
+    Image MgImage;
+
+    float nowFlashTime;
+    public float MgSwitchTime;
+
+    private Color MgStartColor;
+    private Color MgFlashColor;
+
+    bool isFlash;
+
+    void Start()
+    {
+        MgImage = this.gameObject.GetComponent<Image>();
+        MgStartColor = this.gameObject.GetComponent<Image>().color;
+        MgFlashColor = new Color32(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ishaCS.BonusFlg == 1)
+        {
+            if (ishaCS.BonusGaugeSand.fillAmount <= 0.3f)
+            {
+                if (!isFlash)
+                {
+                    isFlash = true;
+                    StartCoroutine(nameof(MgBlinking));
+                    gameSECS.audioSource.PlayOneShot(gameSECS.pause);
+                }
+            }
+        }
+        else
+        {
+            isFlash = false;
+        }
+    }
+
+    private IEnumerator MgBlinking()
+    {
+        float waitTime = MgSwitchTime;
+        nowFlashTime = 0;
+        while (waitTime > nowFlashTime)
+        {
+            nowFlashTime += Time.deltaTime;
+            float rate = nowFlashTime;
+
+            MgImage.color = Color.Lerp(MgStartColor, MgFlashColor, rate);
+            yield return new WaitForFixedUpdate();
+        }
+
+        nowFlashTime = 0;
+        while (waitTime > nowFlashTime)
+        {
+            nowFlashTime += Time.deltaTime;
+            float rate = nowFlashTime * 2f;
+
+            MgImage.color = Color.Lerp(MgFlashColor, MgStartColor, rate);
+            yield return new WaitForFixedUpdate();
+        }
+        isFlash = false;
+    }
+}
---
+++ Assets/ishadou/Script/TimePlus.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePlus : MonoBehaviour
+{
+    [SerializeField] Isha_Singlshot ishaCS;
+    [SerializeField] Text TimeText;
+
+    public float DrawTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TimePlusDraw()
+    {
+        TimeText.text = "+" + " " + ishaCS.TimePlusNum + "s";
+
+        TimeText.gameObject.SetActive(true);
+        StartCoroutine(nameof(TimeFade));
+    }
+
+    private IEnumerator TimeFade()
+    {
+        float waitTime = 0;
+
+        while (waitTime <= DrawTime)
+        {
+            waitTime += Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        TimeText.gameObject.SetActive(false);
+    }
+}

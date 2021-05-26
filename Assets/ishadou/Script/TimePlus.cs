807e23d68f361d7c6ee635970ca5dadb9fa84e5e
CG16-09-PC\GP4_11807128 <11807128@stu.kbc.ac.jp>
2021-05-26 14:21:24 +0900

    加算時間のフェードアウト

--- Assets/ishadou/Script/TimePlus.cs
+++ Assets/ishadou/Script/TimePlus.cs
@@ -9,10 +9,14 @@ public class TimePlus : MonoBehaviour
     [SerializeField] Text TimeText;
 
     public float DrawTime;
+    private Color TpStartColor;
+    private Color TpLastColor;
     // Start is called before the first frame update
     void Start()
     {
         TimeText.gameObject.SetActive(false);
+        TpStartColor = TimeText.color;
+        TpLastColor = new Color(0.3f, 0.5f, 0.5f, 0);
     }
 
     // Update is called once per frame
@@ -25,18 +29,22 @@ public class TimePlus : MonoBehaviour
         TimeText.text = "+" + " " + ishaCS.TimePlusNum + "s";
 
         TimeText.gameObject.SetActive(true);
+
         StartCoroutine(nameof(TimeFade));
     }
 
     private IEnumerator TimeFade()
     {
         float waitTime = 0;
-
+        float rate = 0;
         while (waitTime <= DrawTime)
         {
+            rate = waitTime;
             waitTime += Time.deltaTime;
+            TimeText.color = Color.Lerp(TpStartColor, TpLastColor, rate);
             yield return new WaitForFixedUpdate();
         }
         TimeText.gameObject.SetActive(false);
+        TimeText.color = TpStartColor;
     }
 }
